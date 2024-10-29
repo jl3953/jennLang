8b88d918a9c253e4fc5bae306bedd9521a9cb8d5
Jennifer Lam <jl87@princeton.edu>
2024-10-28 23:22:50 -0400

    install all dependencies for a new cloudlab machine

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+apt update
+apt install htop -y
+apt install bubblewrap -y
+
+# set up vim
+echo 'set nu' >> /root/.vimrc
+echo 'set autoindent' >> /root/.vimrc
+echo 'set tabstop=4 shiftwidth=4 expandtab' >> /root/.vimrc
+
+# install OCaml and Dune
+echo -ne '\n' | bash -c "sh <(curl -fsSL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)" # install to /usr/bin/bash
+yes | opam init
+yes | opam install ocaml-lsp-server odoc ocamlformat utop dune ppx_deriving batteries core
+
+eval $(opam env)
+
+# install Python
+apt install python3 python3-pip
+pip install z3-solver
+
+git clone https://github.com/jl3953/jennLang
