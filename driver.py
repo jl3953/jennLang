ca761ee6e2cdd3a68948b799c6e7c331bb64810c
Jennifer Lam <jl87@princeton.edu>
2024-10-28 23:22:32 -0400

    modify driver to run on bash

--- driver.py
+++ driver.py
@@ -1,7 +1,8 @@
 import argparse
 import subprocess
-import sys
+import sys 
 import datetime
+import time
 
 def main():
     parser = argparse.ArgumentParser(description='Run the checker multiple times')
@@ -17,26 +18,30 @@ def main():
         # print(cmd)
         subprocess.call(cmd, shell=True)
 
-    fails = 0
+    fails = 0 
 
     for i in range(args.trials):
         if i % 100 == 0:
             print("Running trial {0}".format(i))
         dt = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")[:-3]
         interpreter_dump = "output_dump/{0}_dump.txt".format(dt)
-        intermediate_file = "output_dump/{1}_{0}.csv".format(args.intermediate_file, dt)
+        intermediate_file = "output_dump/{1}_{0}.csv".format(args.intermediate_file, dt) 
         linearizability_dump = "output_dump/{0}_lin_dump.txt".format(dt)
-        cmd = "dune exec _build/default/bin/main.exe {0} {1} &> {2}".format(args.spec, intermediate_file, interpreter_dump)
-        # print(cmd)
-        subprocess.call(cmd, shell=True)
-        cmd = "python3 main.py {0} &> {1}".format(intermediate_file, linearizability_dump)
-        # print(cmd)
-        if subprocess.call(cmd, shell=True) != 0:
-            print("Failed on " + intermediate_file)
-            fails = fails + 1
-        else:
-            if (not args.keep_successful_dumps):
-                subprocess.call("rm {0} {1} {2}".format(interpreter_dump, intermediate_file, linearizability_dump), shell=True)
+        cmd = "dune exec _build/default/bin/main.exe {0} {1}".format(args.spec, intermediate_file)
+        print(cmd)
+        with open(interpreter_dump, "w") as outfile:
+            print("interpreter_dump", interpreter_dump)
+            subprocess.run(cmd.split(), stdout=outfile)
+
+        cmd = "python3 main.py {0}".format(intermediate_file)
+        print(cmd)
+        with open(linearizability_dump, "w") as outfile:
+            if subprocess.run(cmd.split(), stdout=outfile) != 0:
+                print("Failed on " + intermediate_file)
+                fails = fails + 1 
+            else:
+                if (not args.keep_successful_dumps):
+                    subprocess.call("rm {0} {1} {2}".format(interpreter_dump, intermediate_file, linearizability_dump), shell=True)
     
     print("Failed {0} out of {1} trials".format(fails, args.trials))
 
